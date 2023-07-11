5438be7f8041fea1e3657aa24b2bb8f1cc3e46ab
Shineast <canfrog@naver.com>
2023-07-11 22:35:51 +0900

    Setting _documet.jsx, pwa

---
+++ app/page/_document.jsx
@@ -0,0 +1,8 @@
+export default function Document() {
+    return (
+        <head>
+            <title>PK English | 온라인 영어 화상회화는 여기서</title>
+            <link rel="manifest" href="/manifest.json" />
+        </head>
+    );
+}
